500fe17364ae807d5aa5afd1ea30174714fbe1d6
areenzo24-meet <139681322+areenzo24-meet@users.noreply.github.com>
2024-07-17 00:07:46 +0300

    lab(not lest)

--- flaskfootoglari/app.py
+++ flaskfootoglari/app.py
@@ -32,7 +32,7 @@ def food1 ():
 
 @app.route('/food2')
 def food2 ():
-  return '''<html><img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUDMhPpndCvGlsF_NF-j1olBZVn9Gg8QDerw&s</html>
+  return '''<html><img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUDMhPpndCvGlsF_NF-j1olBZVn9Gg8QDerw&s></html>
   <a href='/home'>home pj</a>
   <a href='/food3'>food3</a>
   <a href='/pet1'>pet1</a>
@@ -54,6 +54,48 @@ def food3 ():
   <a href='/space1'>space1</a>
   <a href='/space2'>space2</a>
   <a href='/space3'>space3</a>'''
+
+
+@app.route('/pet1')
+def pet1 ():
+  return'''<html><img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3HTgKkm-tBcNbiLG5eCL12nOngY_fEta2BA&s></html>
+  <a href='/home'>home pj</a>
+  <a href='/pet2'>pet2</a>
+  <a href='/pet3'>pet3</a>
+  <a href='/space1'>space1</a>
+  <a href='/space2'>space2</a>
+  <a href='/space3'>space3</a>'''
+
+
+@app.route('/pet2')
+def pet2 ():
+  return'''<html><img src=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8q8tDRNSGOaBOUTKecyfAAGzv-HR-Nv8Pag&s></html>
+  <a href='/home'>home pj</a>
+  <a href='/pet3'>pet3</a>
+  <a href='/space1'>space1</a>
+  <a href='/space2'>space2</a>
+  <a href='/space3'>space3</a>'''
+
+
+@app.route('/pet3')
+def pet3 ():
+  return'''<html><img src=https://cdn.pixabay.com/photo/2023/12/10/03/00/peacock-8440548_640.jpg></html>
+  <a href='/home'>home pj</a>
+  <a href='/space1'>space1</a>
+  <a href='/space2'>space2</a>
+  <a href='/space3'>space3</a>'''
+
+
+ @app.route('/space1')
+def space1 ():
+  return'''<html><img src=https://images.pexels.com/photos/41162/
+  moon-landing-apollo-11-nasa-buzz-aldrin-41162.jpeg?cs=srgb&dl=pexels-pixabay
+  -41162.jpg&fm=jpg></html>
+  <a href='/home'>home pj</a>
+  <a href='/space2'>space2</a>
+  <a href='/space3'>space3</a>'''
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
