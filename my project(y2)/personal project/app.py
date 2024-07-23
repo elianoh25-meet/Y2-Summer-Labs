6103cdc547b5158d4e2fa6aeda537a2485e30935
areenzo24-meet <139681322+areenzo24-meet@users.noreply.github.com>
2024-07-23 18:19:26 +0300

    sign in/sign up/background/plce to the <p>
    need to add incide the <p>
    move to the side

---
+++ my project(y2)/personal project/app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import session 
+import pyrebase
+
+Config = {
+  "apiKey": "AIzaSyDiG8xWZsHS8DqwP2nobYJoOK0h8-f5yCU",
+  "authDomain": "personal-project-cd203.firebaseapp.com",
+  "projectId": "personal-project-cd203",
+  "storageBucket": "personal-project-cd203.appspot.com",
+  "messagingSenderId": "367460617784",
+  "appId": "1:367460617784:web:a58b5e509c677912bf60cd",
+  "databaseURL":""
+}
+
+firebase = pyrebase.initialize_app(Config)
+auth = firebase.auth()
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = 'super-secret-key'
+
+@app.route('/home')
+def home():
+	return render_template("home.html")
+
+#main route--> signin 
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        name = request.form['name']
+        try:
+            session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('home'))
+            
+        except :
+            error = "Authentication failed"
+            print(error)
+            return redirect(url_for('signin')) 
+
+    elif request.method == 'GET':
+    	return render_template("signin.html")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def signup():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        name = request.form['name']
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            session['user']= user
+            return redirect(url_for('home'))
+        except :
+            error = "Authentication failed"
+            print(error)
+    return render_template("signup.html")
+
+
+
+@app.route('/signout')
+def signout():
+    login_session["user"]=None
+    auth.current_user = None
+    login_session.modified = True
+    return redirect(url_for('signin'))
+    return 
+
+
+
+if __name__ == '__main__':
+ 
+    app.run( debug=True, port=5051)
---
+++ my project(y2)/personal project/templates/untitled.py
@@ -0,0 +1,25 @@
+#app.Config['SECRET_KEY'] = 'super-secret-key'
+
+#@app.route('/', methods=["GET", "POST"])
+#def login():
+    #if request.method == 'GET':
+       # return render_template("signup.html") 
+    #else: #if the method is post
+      #  email = request.form['email']
+       # password = request.form['password']
+
+
+
+#@app.route('/signin', methods=['GET', 'POST'])
+#def signin():
+  #  if request.method == 'GET':
+   #     return render_template("login.html") 
+    #else: #if the method is post
+       # email = request.form['email']
+       # password = request.form['password']
+
+
+
+
+#firebase = pyrebase.initialize_app(Config)
+#auth = firebase.auth()
